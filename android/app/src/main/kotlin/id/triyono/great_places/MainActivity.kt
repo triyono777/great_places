f163159fe3707dbf5609ee2372f03773ba75b7ca
triyono <yono.tri@gmail.com>
2020-05-21 22:55:19 +0700

    init pertama

---
+++ android/app/src/main/kotlin/id/triyono/great_places/MainActivity.kt
@@ -0,0 +1,6 @@
+package id.triyono.great_places
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:great_places/screens/places_list_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Greates Places',
+      theme: ThemeData(primarySwatch: Colors.indigo, accentColor: Colors.amber),
+      home: PlacesListScreen(),
+    );
+  }
+}
